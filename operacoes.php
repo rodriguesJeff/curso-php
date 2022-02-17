366ed33d28c5a8cd5de80e64b2c7dc1d6dbc79c5
Jefferson Rodrigues <rodriguesjeff.dev@gmail.com>
2022-02-17 18:53:51 -0300

    Tipos primitivos

---
+++ operacoes.php
@@ -0,0 +1,17 @@
+<?php
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
+$doisAoCubo = 2 ** 3;
+
+$restoDaDivisao = 10 % 2;
+
+echo $soma;
+echo $subtracao;
+echo $multiplicacao;
+echo $divisao;
+echo $doisAoCubo;
+echo $restoDaDivisao;
---
+++ tipos.php
@@ -0,0 +1,11 @@
+<?php
+
+$idade = 21;
+$salario = 6200.20;
+$divisao = 10 / 3;
+
+$texto = "Jefferson";
+
+$verdadeiro = true;
+
+echo gettype($verdadeiro);
