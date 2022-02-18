67366676a1d4bcc6000b40693632016d24576e2b
Jefferson Rodrigues <rodriguesjeff.dev@gmail.com>
2022-02-18 14:16:35 -0300

    trabalhando com foreach

---
+++ avancando/foreach.php
@@ -0,0 +1,23 @@
+<?php
+
+$contas = [
+    11111111111 => [
+        'titular' => 'Jefferson',
+        'saldo' => 2000, 
+    ], 
+    22222222222 => [
+        'titular' => 'Maria',
+        'saldo' => 10000,
+    ], 
+    33333333333 => [
+        'titular' => 'Alberto',
+        'saldo' => 300,
+    ],
+];
+
+foreach ($contas as $cpf => $conta) {
+    echo "Conta do " . $conta['titular'] . "\n";
+    echo "Saldo: R\$ " . $conta['saldo'] . "\n";
+    echo "CPF do cliente: " . $cpf . "\n";
+    echo "\n";
+} 
