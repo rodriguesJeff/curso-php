d077780eb2204b137f801ed324cd2fb6a96fdfbd
Jefferson Rodrigues <rodriguesjeff.dev@gmail.com>
2022-02-21 10:30:20 -0300

    Adicionada tipagem

--- avancando/banco.php
+++ avancando/banco.php
@@ -1,6 +1,6 @@
 <?php 
 
-function sacar($conta, $valor) {
+function sacar(array $conta, float $valor) {
     if ($valor > $conta['saldo']){
         exibeMensagem("Voce nao pode sacar");
     } else {
@@ -10,8 +10,18 @@ function sacar($conta, $valor) {
     return $conta; // funcao devolve algum valor novo
 }
 
-function exibeMensagem($mensagem) {
+function exibeMensagem(string $mensagem) {
     echo $mensagem . PHP_EOL; // sub rotina nao retorna nada
+}
+
+function depositar(array $conta, float $valor) : array {
+    if ($valor > 0) {
+        $conta['saldo'] += $valor;
+    } else {
+        exibeMensagem("Depositos tem que ser positivos");
+    }
+    
+    return  $conta;
 }
 
 $contas = [
@@ -30,7 +40,17 @@ $contas = [
 ];
 
 $contas[11111111111] = sacar($contas[11111111111], 500);
-$contas[22222222222] = sacar($contas[22222222222], 300000);
+$contas[22222222222] = sacar($contas[22222222222], 50);
+
+$contas[11111111111] = depositar(
+    $contas[11111111111],
+    7000
+);
+
+$contas[22222222222] = depositar(
+    $contas[22222222222],
+    40
+);
 
 foreach ($contas as $cpf => $conta) {
     echo "Conta do " . $conta['titular'] . "\n";
