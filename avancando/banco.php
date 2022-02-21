efc76271e830197dc64b80b957568193101f67de
Jefferson Rodrigues <rodriguesjeff.dev@gmail.com>
2022-02-21 11:28:43 -0300

    include, require e require_once

--- avancando/banco.php
+++ avancando/banco.php
@@ -1,28 +1,8 @@
 <?php 
 
-function sacar(array $conta, float $valor) {
-    if ($valor > $conta['saldo']){
-        exibeMensagem("Voce nao pode sacar");
-    } else {
-        $conta['saldo'] -= $valor;
-    }
-
-    return $conta; // funcao devolve algum valor novo
-}
-
-function exibeMensagem(string $mensagem) {
-    echo $mensagem . PHP_EOL; // sub rotina nao retorna nada
-}
-
-function depositar(array $conta, float $valor) : array {
-    if ($valor > 0) {
-        $conta['saldo'] += $valor;
-    } else {
-        exibeMensagem("Depositos tem que ser positivos");
-    }
-    
-    return  $conta;
-}
+// include 'funcoes.php';
+// require 'funcoes.php';
+require_once 'funcoes.php';
 
 $contas = [
     11111111111 => [
---
+++ avancando/funcoes.php
@@ -0,0 +1,25 @@
+<?php
+
+function sacar(array $conta, float $valor) {
+    if ($valor > $conta['saldo']){
+        exibeMensagem("Voce nao pode sacar");
+    } else {
+        $conta['saldo'] -= $valor;
+    }
+
+    return $conta; // funcao devolve algum valor novo
+}
+
+function exibeMensagem(string $mensagem) {
+    echo $mensagem . PHP_EOL; // sub rotina nao retorna nada
+}
+
+function depositar(array $conta, float $valor) : array {
+    if ($valor > 0) {
+        $conta['saldo'] += $valor;
+    } else {
+        exibeMensagem("Depositos tem que ser positivos");
+    }
+    
+    return  $conta;
+}
